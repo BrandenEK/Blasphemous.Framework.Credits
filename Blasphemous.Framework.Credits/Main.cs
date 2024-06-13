8b25ae694f16c80aa01ee366189b4ed6c6202f83
unknown <brandenek03912@hotmail.com>
2024-06-13 14:24:41 -0400

    init: add base project files

---
+++ Blasphemous.Framework.Credits/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Framework.Credits;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+internal class Main : BaseUnityPlugin
+{
+    public static Credits Credits { get; private set; }
+
+    private void Start()
+    {
+        Credits = new Credits();
+    }
+}
