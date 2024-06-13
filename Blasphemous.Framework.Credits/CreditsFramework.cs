172ff52c8bbd4a825a1ad91d29a4875fcb87d2df
unknown <brandenek03912@hotmail.com>
2024-06-13 14:28:31 -0400

    core: implement project settings

---
+++ Blasphemous.Framework.Credits/CreditsFramework.cs
@@ -0,0 +1,16 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.Framework.Credits;
+
+/// <summary>
+/// Handles appending sections to the game credits
+/// </summary>
+public class CreditsFramework : BlasMod
+{
+    internal CreditsFramework() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
+}
--- Blasphemous.Framework.Credits/Main.cs
+++ Blasphemous.Framework.Credits/Main.cs
@@ -3,13 +3,13 @@
 namespace Blasphemous.Framework.Credits;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.2.0")]
 internal class Main : BaseUnityPlugin
 {
-    public static Credits Credits { get; private set; }
+    public static CreditsFramework CreditsFramework { get; private set; }
 
     private void Start()
     {
-        Credits = new Credits();
+        CreditsFramework = new CreditsFramework();
     }
 }
