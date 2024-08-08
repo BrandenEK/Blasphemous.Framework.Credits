22e7f156f75a0012cdc9a6587a1e7001078f45f6
unknown <brandenek03912@hotmail.com>
2024-08-08 11:45:44 -0400

    core: reference new libs

--- Blasphemous.Framework.Credits/Main.cs
+++ Blasphemous.Framework.Credits/Main.cs
@@ -3,7 +3,7 @@
 namespace Blasphemous.Framework.Credits;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.2.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.4.1")]
 internal class Main : BaseUnityPlugin
 {
     public static CreditsFramework CreditsFramework { get; private set; }
